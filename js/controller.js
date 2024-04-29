4d822b156e79ecc2f0857c002f1cb0eea063566d
Miki Gerlašinský <m11@m11.cz>
2024-04-29 15:39:13 +0200

    drobné změny

--- js/controller.js
+++ js/controller.js
@@ -7,7 +7,7 @@ class Controller {
     }
 
     startRender() {
-        this.view.render(this.model.toDayLabel);
+        this.view.render(this.model.todayLabel);
     }
 }
 
--- js/model.js
+++ js/model.js
@@ -2,21 +2,21 @@
 
 class Model {
 
-    get toDayLabel() {
+    get todayLabel() {
         this.dateString = new CalendarCalculator();
-        return this.dateString.toDayLabel;
+        return this.dateString.todayLabel;
         
     }
 }
 
 class CalendarCalculator {
     constructor (){
-        this.calCalc = new Date(Date());
-        this.options = { weekday: "long" };
+        this.calCalc = new Date();
         this.weekDay = new Intl.DateTimeFormat("cz-CZ", { weekday: "long" }).format(this.calCalc);
+        
     }
 
-    get toDayLabel() {
+    get todayLabel() {
         return this.weekDay + " " + this.calCalc.toLocaleDateString();
     }
 }
