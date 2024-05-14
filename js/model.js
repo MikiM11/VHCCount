464735244d3cb2ae0baeafb3689c1472be0061cd
Miki Gerlašinský <m11@m11.cz>
2024-05-14 14:13:37 +0200

    pokus

--- js/model.js
+++ js/model.js
@@ -20,3 +20,7 @@ class CalendarCalculator {
         return this.weekDay + " " + this.calCalc.toLocaleDateString();
     }
 }
+
+const url = "https://script.google.com/macros/s/AKfycbxk9eEdN1dija4QZaSXM8MQPZgYFdCrm14_gpDbojNiHf7gYPCMeN1_qQDoG6xs1jX1OA/exec"
+
+fetch(url);
